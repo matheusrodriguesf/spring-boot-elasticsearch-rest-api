ae2ddcf5606e470175794150b5484729e6ee0cd2
matheusrodriguesf <matheus@DESKTOP-I67L4GE.localdomain>
2020-07-30 15:58:51 -0300

    Modelo de dados

---
+++ src/main/java/com/matheus/elasticsearchrestapi/models/FakerJob.java
@@ -0,0 +1,20 @@
+package com.matheus.elasticsearchrestapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FakerJob {
+	private String title;
+	private String location;
+	private String companyProfile;
+	private String description;
+	private String requirements;
+	private String benefits;
+	private String employmentType;
+	private String requiredExperience;
+	private String industry;
+}
