b66618895af574e41a1c1e66f2c745850c88444f
matheusrodriguesf <matheus@DESKTOP-I67L4GE.localdomain>
2020-07-30 13:11:40 -0300

    importacao projeto

---
+++ src/main/java/com/matheus/elasticsearchrestapi/ElasticsearchrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.elasticsearchrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/elasticsearchrestapi/ElasticsearchrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.elasticsearchrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
